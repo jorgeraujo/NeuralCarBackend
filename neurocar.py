876d3922633b11ffa49e1932ea00625eab070e75
Jorge Araújo <jdcmaraujo@gmail.com>
2018-04-22 00:03:39 +0100

    adding the Best Of - The CORS

--- neurocar.py
+++ neurocar.py
@@ -2,15 +2,16 @@ from flask import Flask
 from flask import jsonify
 from os import system
 import label_image
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/')
 def index():
     return "Hell, World!"
 
-@app.route('/recognize', methods=['GET'])
+@app.route('/recognize', methods=['POST'])
 def recognize_car():
     return jsonify((label_image.call_comparation_function("teste_mt07.jpg")))
 
