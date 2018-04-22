824529e1696ee20e1111fbe1843ac41603d70a6f
Jorge Araújo <jdcmaraujo@gmail.com>
2018-04-22 06:09:46 +0100

    jdsfasd

--- neurocar.py
+++ neurocar.py
@@ -39,7 +39,8 @@ def recognize_car():
         file.write(i.content)
     result = label_image.call_comparation_function(img_name)
     so = wptools.page(brand_models[result[0][0]][0] + ' ' + brand_models[result[0][0]][1]).get_parse()
-    return jsonify({"brand":brand_models[result[0][0]][0],"model":brand_models[result[0][0]][1],"accuracy":result[0][1],"so":so})
+    soN = json.dumps(so.__dict__)
+    return jsonify({"brand":brand_models[result[0][0]][0],"model":brand_models[result[0][0]][1],"accuracy":result[0][1],"so":soN})
 
 
 if __name__ == '__main__':
