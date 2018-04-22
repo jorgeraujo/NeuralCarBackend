c47b21ef4cfded5da28ef5206e9c73513958c5e6
Jorge Araújo <jdcmaraujo@gmail.com>
2018-04-22 01:32:27 +0100

    requirements updated

--- neurocar.py
+++ neurocar.py
@@ -1,8 +1,13 @@
 from flask import Flask
-from flask import jsonify
+from flask import jsonify, request
 from os import system
 import label_image
 from flask_cors import CORS
+import requests
+from io import open as iopen
+from urlparse import urlsplit
+import urllib as urllib
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -13,7 +18,15 @@ def index():
 
 @app.route('/recognize', methods=['POST'])
 def recognize_car():
-    return jsonify((label_image.call_comparation_function("teste_mt07.jpg")))
+    data = request.data
+    dataDict = json.loads(data)
+    print(dataDict["url"])
+    url = dataDict["url"]
+    img_name = "image.jpg"
+    f = open(img_name,'wb')
+    f.write(urllib.urlopen(url).read())
+    f.close()
+    return jsonify((label_image.call_comparation_function(img_name)))
 
 
 if __name__ == '__main__':
